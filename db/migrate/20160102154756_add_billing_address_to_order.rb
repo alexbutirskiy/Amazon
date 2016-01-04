0d39efef2651fe9db7ad72f7d9f575c740af8586
Alex Butirskiy <butirskiy@gmail.com>
2016-01-04 07:51:02 +0200

    Project empied

--- db/migrate/20160102154756_add_billing_address_to_order.rb
+++
@@ -1,5 +0,0 @@
-class AddBillingAddressToOrder < ActiveRecord::Migration
-  def change
-    add_reference :orders, :billing_address, index: true, foreign_key: true
-  end
-end
--- spec/factories/authors.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :author do
-    firstname { Faker::Name.first_name }
-    lastname { Faker::Name.last_name }
-    biography { Faker::Lorem.paragraph }
-  end
-
-end
