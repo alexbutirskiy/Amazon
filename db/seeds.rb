90868ee9d89fd4c992372ea3b5e3c12e5f03d460
Alex Butirskiy <butirskiy@gmail.com>
2016-01-15 16:58:17 +0200

    Seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if Rails.env == "development"
+  20.times do
+    Category.create title: Faker::Book.genre
+  end
+
+  50.times do
+    Author.create(
+      firstname: Faker::Name.first_name,
+      lastname: Faker::Name.last_name,
+      biography: Faker::Lorem.paragraph)
+  end
+
+  500.times do
+    b = Book.create(
+      category: Category.all.sample,
+      title: Faker::Book.title,
+      price: (Faker::Commerce.price+1),
+      description: Faker::Lorem.paragraph)
+
+    rand(1..3).times do
+      b.authors << Author.all.sample
+    end
+  end
+end
