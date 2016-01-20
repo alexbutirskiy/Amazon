b27390f9afdf5a3e3966033774f68edb1255f57c
Alex Butirskiy <butirskiy@gmail.com>
2016-01-20 18:44:06 +0200

    Books view added

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -5,4 +5,31 @@ module BooksHelper
       out + link_to("#{ author.name() }", author_path(author))
     end
   end
+
+  def authors_names(authors)
+    authors.inject('') do |out, a|
+      out += ', ' unless out.empty?
+      out += a.name
+    end
+  end
+
+  def bestseller_prev_link
+    if @id == 1
+      content_tag(:p, '', class: 'fa fa-caret-left arrow inactive')
+    else
+      link_to('', bestseller_path(@id - 1), class: 'fa fa-caret-left arrow active')            
+    end
+  end
+
+  def bestseller_next_link
+    if @id == Book.bestsellers_max
+      content_tag(:p, '', class: 'fa fa-caret-right arrow inactive')
+    else
+      link_to('', bestseller_path(@id + 1), class: 'fa fa-caret-right arrow active')
+    end
+  end
+
+  def display_books
+    
+  end
 end
