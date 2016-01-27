7356bf04b9d2ed62639e19812df10d2431a54dd6
Alex Butirskiy <butirskiy@gmail.com>
2016-01-27 12:16:48 +0200

    Little style improvement

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,8 +2,6 @@ class Customer < ActiveRecord::Base
   has_many :orders
   has_many :ratings, -> { where value: 4 }
 
-  # validates :email, presence: true, uniqueness: { case_sensitive: false }
-  # validates :password, presence: true
   validates :firstname, presence: true
   validates :lastname, presence: true
 
