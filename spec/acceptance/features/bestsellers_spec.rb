95d18a36b0948ab0c2bfdfab0ca400ea4e83eda7
Alex Butirskiy <butirskiy@gmail.com>
2016-01-27 11:00:11 +0200

    Some fixes for bestsellers feature test

--- spec/acceptance/features/bestsellers_spec.rb
+++ spec/acceptance/features/bestsellers_spec.rb
@@ -3,7 +3,7 @@ require 'acceptance/features/features_spec_helper'
 
 feature "Bestsellers" do
   context 'when DB is empty' do
-    scenario "Shows 'There is no books in the Store'" do
+    it "Shows 'There is no books in the Store'" do
       visit "/books/bestsellers/1"
       expect(page).to have_content('There is no books in the Store')
     end
@@ -12,40 +12,59 @@ feature "Bestsellers" do
   context 'when DB has book data' do
     before do
       @book = create(:book)
+      visit bestseller_path(1)
     end
 
-    scenario 'Book title is displayed' do
-      visit bestseller_path(1)
+    it 'Book title is displayed' do
       expect(page).to have_content(@book.title)
     end
 
-    scenario "Book authors' links are displayed" do
-      visit bestseller_path(1)
+    it "Book authors' links are displayed" do
       @book.authors.each do |author|
         expect(page).to have_link("#{author.name}", href: "/authors/#{author.id}")
       end
     end
 
-    scenario 'Book description is displayed' do
-      visit bestseller_path(1)
+    it 'Book description is displayed' do
       expect(page).to have_content(@book.description)
     end
 
-    scenario 'Book price is displayed' do
-      visit bestseller_path(1)
+    it 'Book price is displayed' do
       expect(page).to have_content(@book.price)
     end
+  end
 
-    # context "on 'books/bestsellers/1' url" do
-    #   before do
-    #     create(:book, sold: 1)
-    #     create(:book, sold: 2)
-    #     create(:book, sold: 3)
-    #   end
-    #   scenario "Shows the best-selling book" do
-    #     vivsit "/books/bestsellers/1"
-    #     expect()
-    #   end
-    # end
+  context 'when 1st bestseller is displayed' do
+    before do
+      create(:book)
+      create(:book)
+      create(:book)
+      visit bestseller_path(1)
+    end
+
+    it 'displays left arrow as innactive' do
+      expect(page.find('.fa-caret-left')[:class].split).to include('inactive')
+    end
+
+    it 'displays right arrow as active' do
+      expect(page.find('.fa-caret-right')[:class].split).to include('active')
+    end
+  end
+
+  context 'when last bestseller is displayed' do
+    before do
+      create(:book)
+      create(:book)
+      create(:book)
+      visit bestseller_path(3)
+    end
+
+    it 'displays left arrow as nactive' do
+      expect(page.find('.fa-caret-left')[:class].split).to include('active')
+    end
+
+    it 'displays right arrow as inactive' do
+      expect(page.find('.fa-caret-right')[:class].split).to include('inactive')
+    end
   end
 end
