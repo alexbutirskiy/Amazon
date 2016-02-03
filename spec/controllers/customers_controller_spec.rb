64d353d3ccc98b39b43ee5069d80f8098a374d78
Alex Butirskiy <butirskiy@gmail.com>
2016-02-03 12:26:32 +0200

    Some bugs in CustomersControlle spec fixed

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -1,49 +1,53 @@
 require 'rails_helper'
 
-def test_addresses
-  it 'sets billing and shipping addresses' do
-    expect(controller).to receive(:set_addresses)
-    get(:edit, user_id: @user.id)
-  end
-end
-
 RSpec.describe CustomersController, type: :controller do
+  let(:customer) { build(:customer) }
+  let(:user) { create(:user, customer: customer) }
+
   before(:each) do
-    @user = create(:user, customer: create(:customer))
+    #@user = create(:user, customer: create(:customer))
+    allow(User).to receive(:find).with(user.id.to_s).and_return(user)
     @request.env["devise.mapping"] = Devise.mappings[:user]
-    sign_in @user
+    sign_in user
   end
 
   describe "GET #edit" do
-    before(:each) do
-      get(:edit, user_id: @user.id)
-    end
-
     context "when User has Customer profile registered" do
       it 'assigns @user' do
-        expect(assigns(:user)).to eq @user
+        get(:edit, user_id: user.id)
+        expect(assigns(:user)).to eq user
       end
 
       it 'assigns @customer' do
-        expect(assigns(:customer)).to eq @user.customer
+        get(:edit, user_id: user.id)
+        expect(assigns(:customer)).to eq user.customer
       end
 
-      test_addresses
+      it 'sets billing and shipping addresses' do
+        expect(controller).to receive(:set_addresses)
+        get(:edit, user_id: user.id)
+      end
     end
 
     context "when User has not registered Customer profile" do
       before(:each) do
-        @user.update_attribute(:customer, nil)
+        user.update_attribute(:customer, nil)
       end
 
       it 'assigns @customer' do
-        CUSTOMER = 'Customer'
-        expect(Customer).to receive(:new).and_return(CUSTOMER)
-        get(:edit, user_id: @user.id)
-        expect(assigns(:customer)).to eq CUSTOMER
+        expect(Customer).to receive(:new).and_return(customer)
+        get(:edit, user_id: user.id)
+        expect(assigns(:customer)).to eq customer
       end
 
-      test_addresses
+      it 'sets billing and shipping addresses' do
+        expect(controller).to receive(:set_addresses)
+        get(:edit, user_id: user.id)
+      end
     end
+  end
+
+  describe 'POST #create' do
+
   end
 end
